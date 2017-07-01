14f38ed7ff60a46d7465fd726151a5bc5359e5c1
Diogo Silva <dbtdsilva@gmail.com>
2017-07-01 02:09:24 +0100

    Url: Replaced usages from http decode/encode with the Url static class

--- src/CloudProvider/OneDrive.cpp
+++ src/CloudProvider/OneDrive.cpp
@@ -55,7 +55,7 @@ ICloudProvider::UploadFileRequest::Pointer OneDrive::uploadFileAsync(
         [=](std::ostream&) {
           return http()->create(
               endpoint() + "/v1.0/drive/items/" + parent->id() + ":/" +
-                  http()->escape(filename) + ":/upload.createSession",
+                  util::Url::escape(filename) + ":/upload.createSession",
               "POST");
         },
         output);
